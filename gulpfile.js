a87b737e68627fd22c0bb33812d1fa0cbb8d2ade
Alex <alexvictar@mail.ru>
2020-08-10 11:41:20 +0600

    first section

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,9 @@ gulp.task('server', function() {
 });
 
 gulp.task('styles', function() {
-    return gulp.src("sass/*.+(scss|sass)")
+    return gulp.src('sass/**/*.+(scss|sass)')
+    // return gulp.src('sass/base/*.+(scss|sass)')
+    // return gulp.src('sass/blocks/*.+(scss|sass)')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
@@ -27,7 +29,7 @@ gulp.task('styles', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch("sass/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("sass/**/*.+(scss|sass)", gulp.parallel('styles'));
 })
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
