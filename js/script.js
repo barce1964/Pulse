d37b7f4c507a18600b3a71266d1def34890a64dc
Alex <alexvictar@mail.ru>
2020-08-13 17:41:21 +0600

    tiny slider

--- js/script.js
+++ js/script.js
@@ -1,27 +1,67 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        //arrows: false,
-        speed: 1500,
-        // slidesToShow: 1,
-        // autoplay: true,
-        // autoplaySpeed: 2000,
-        // fade: true,
-        // cssEase: 'linear',
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/prev.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/next.png"></button>',
-        responsive: [
-            {
-                breakpoint: 991,
-                settings: {
-                    // dots: true,
-                    arrows: false,
-                    slidesToShow: 1,
-                    autoplay: true,
-                    autoplaySpeed: 2000,
-                    fade: true,
-                    cssEase: 'linear'
-                }
-            }
-        ]
-      });
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         //arrows: false,
+//         speed: 1500,
+//         // slidesToShow: 1,
+//         // autoplay: true,
+//         // autoplaySpeed: 2000,
+//         // fade: true,
+//         // cssEase: 'linear',
+//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/prev.png"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="icons/next.png"></button>',
+//         responsive: [
+//             {
+//                 breakpoint: 991,
+//                 settings: {
+//                     // dots: true,
+//                     arrows: false,
+//                     slidesToShow: 1,
+//                     autoplay: true,
+//                     autoplaySpeed: 2000,
+//                     fade: true,
+//                     cssEase: 'linear'
+//                 }
+//             }
+//         ]
+//       });
+//   });
+
+
+
+const slider = tns({
+    container: '.carousel__inner',
+    //gutter: 30,
+    items: 1,
+    slideBy: 'page',
+    controls: false,
+    autoplay: false,
+    nav: false, 
+    speed: 2000,
+    responsive: {
+        575: {
+            gutter: 15,
+            items: 1,
+            touch: true
+         },
+        767: {
+            gutter: 20,
+            items: 1
+         },
+        991: {
+            gutter: 25,
+            items: 1
+         },
+        1200: {
+            gutter: 30,
+            items: 1
+        }
+     }
+});
+
+document.querySelector('.prev').addEventListener('click', function() {
+    slider.goTo('prev');
+  });
+
+document.querySelector('.next').addEventListener('click', function() {
+    slider.goTo('next');
   });
